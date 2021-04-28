72b63fb8c76b6f5e55f1b4a40a11fc6bd31d9a72
AsheOmFree <75257331+AsheOmFree@users.noreply.github.com>
2021-04-28 17:43:08 -0400

    Add files via upload

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Header() {
+    return (
+        <header>
+            <img 
+                src="https://tattoodo-mobile-app.imgix.net/images/news_uploads/legacy/0/118187.jpg?auto=format%2Ccompress&auto=format%2Ccompress&w=1600" 
+                alt="Problem?"
+            />
+            <p>Meme Generator</p>
+        </header>
+    )
+}
+
+export default Header
